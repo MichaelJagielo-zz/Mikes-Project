2a0a99298d3684dd1ff0f2dda13084471c88582a
Michael Morris <morris4019@gmail.com>
2012-07-18 16:36:22 -0700

    Updated Point3D.java.

--- src/com/stickmangame/Point3D.java
+++ src/com/stickmangame/Point3D.java
@@ -1,11 +1,15 @@
 package com.stickmangame;
-public class Point3D extends Point2D.Float {
-	float z;
+
+public class Point3D {
+	
+	public float x;
+	public float y;
+	public float z;
 	
 	public Point3D (float x, float y, float z) {
-		super(x,y);
+		this.x = x;
+		this.y = y;
 		this.z = z;
-
 	}
 
 }
